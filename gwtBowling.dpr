6fe775622946c084417427f4874fc87d7671b431
sysop@tgp.net <eisc0013>
2019-08-05 21:41:19 -0500

    Many modifications, a bit of brute-force iterating on the algorithm.

--- gwtBowling.dpr
+++ gwtBowling.dpr
@@ -14,6 +14,36 @@ begin
   try
     { TODO -oUser -cConsole Main : Insert code here }
     GameBowling := TGame.Create;
+
+    // ALE 20190805 throw some balls
+    while GameBowling.CurrentFrame < 10 do
+    begin
+      GameBowling.Roll;
+    end;
+
+    // ALE 20190805 show some results
+    WriteLn('Score: ' + GameBowling.TotalScore.ToString + ' Rolls: ' + GameBowling.TotalRolls.ToString);
+    for var I := 1 to 10 do
+    begin
+      WriteLn('Frame: ' + GameBowling.ScoreByFrame[I].number.ToString
+       + ' Rolls: ' + GameBowling.ScoreByFrame[I].rolls.ToString
+       + ' Score: ' + GameBowling.ScoreByFrame[I].score.ToString);
+      for var J := 1 to GameBowling.ScoreByFrame[I].rolls do
+      begin
+        WriteLn('  Roll ' + J.ToString + ' Pins Downed: ' + GameBowling.ScoreByFrame[I].roll[J].ToString);
+      end;
+
+    end;
+
+    {
+    for var I := 0 to 22 do
+    begin
+      WriteLn(IntToStr(GameBowling.Roll) + ' ' + IntToStr(GameBowling.ScoreByFrame[GameBowling.CurrentFrame].rolls)
+       + ' ' + IntToStr(GameBowling.ScoreByFrame[GameBowling.CurrentFrame].number)
+       + ' ' + IntToStr(GameBowling.TotalScore)
+       + ' ' + IntToStr(GameBowling.TotalRolls));
+    end;
+    }
   except
     on E: Exception do
       Writeln(E.ClassName, ': Could not create GameBowling', E.Message);
--- uBowling.pas
+++ uBowling.pas
@@ -2,17 +2,31 @@ unit uBowling;
 
 interface
 
-type
+  const
+    FRAMES_TOTAL = 10;
+    ROLLS_PER_FRAME_MAX = 3;
+
+  type
   { ALE 20190805 What do we want to know about a Frame?
     1. 1st Roll result, 2. 2nd Roll result, 3. 3rd Roll result
     4. Frame # }
+  TFrame = record
+    number: Integer;
+    rolls: Integer;
+    score: Integer;
+    roll: array[1..ROLLS_PER_FRAME_MAX] of Integer;
+  end;
+  TGameOfFrames = array [1..FRAMES_TOTAL] of TFrame;
+
   IGame = interface ['{A1E0B9A0-061A-40FF-8E57-0AD401FCAF8E}']
     function Start: Boolean;
     function Roll: Integer; Overload;
     function Roll(APinsDown: Integer): Integer; Overload;
-    function ScoreByFrame: Boolean;
+    function ScoreByFrame: TGameOfFrames;
     function GetTotalScore: Integer;
     function GetTotalRolls: Integer;
+    function GetCurrentFrame: Integer;
+    property CurrentFrame: Integer read GetCurrentFrame;
     property TotalScore: Integer read GetTotalScore;
     property TotalRolls: Integer read GetTotalRolls;
   end;
@@ -21,6 +35,9 @@ type
   private
     FScore: Integer;
     FRolls: Integer; // ALE 20190805 Counting rolls for fun
+    FFrame: Integer;
+    FFrames: TGameOfFrames;
+    function GetCurrentFrame: Integer;
     function GetTotalScore: Integer;
     function GetTotalRolls: Integer;
   public
@@ -28,7 +45,7 @@ type
     function Start: Boolean;
     function Roll: Integer; Overload;
     function Roll(APinsDown: Integer): Integer; Overload;
-    function ScoreByFrame: Boolean;
+    function ScoreByFrame: TGameOfFrames;
   end;
 implementation
 
@@ -39,7 +56,13 @@ begin
   Randomize; // ALE 20190805 Set up random number generator
   FScore := -1;
   FRolls := -1;
+  FFrame := -1;
   Start; // ALE 20190805 Do we really want to do this or just initialize in Create?
+end;
+
+function TGame.GetCurrentFrame: Integer;
+begin
+  result := FFrame;
 end;
 
 function TGame.GetTotalRolls: Integer;
@@ -54,25 +77,80 @@ end;
 
 function TGame.Roll: Integer;
 begin
-  result := Roll(Random(10));
+  if (FFrame < FRAMES_TOTAL) then
+  begin
+    if FFrames[FFrame].rolls = 0 then
+    begin
+      result := Roll(Random(11));
+    end
+    else
+    begin
+      result := Roll(Random(11 - FFrames[FFrame].roll[1]));
+    end;
+  end
+  else
+  begin
+    { TODO -oUser -cShould only be able to roll as many pins as are still standing }
+    result := Roll(Random(11));
+  end;
+
+
 end;
 
 function TGame.Roll(APinsDown: Integer): Integer;
+var
+  lRolls: Integer;
 begin
   Inc(FScore, APinsDown);
   Inc(FRolls);
-  result := FScore;
+  Inc(FFrames[FFrame].rolls);
+  lRolls := FFrames[FFrame].rolls;
+
+  FFrames[FFrame].roll[lRolls] := APinsDown;
+
+  if ((FFrames[FFrame].roll[1] = 10) AND (FFrame < FRAMES_TOTAL)) then
+  begin
+    // ALE 20190805 got a strike, move us to next frame for all but 10th frame
+    Inc(FFrame);
+  end
+  else if ((FFrames[FFrame].rolls = 2) AND (FFrame < FRAMES_TOTAL)) then
+  begin
+    // ALE 20190805 rolled two balls, move us to next frame for all but 10th frame
+    Inc(FFrame);
+  end;
+
+  // ALE 20190805 calculate score, use look-back to previous frames if needed
+
+  result := APinsDown;
+  { TODO -oUser -cShould only be able to roll as many pins as are still standing }
 end;
 
-function TGame.ScoreByFrame: Boolean;
+function TGame.ScoreByFrame: TGameOfFrames;
 begin
-  result := False;
+  result := FFrames;
 end;
 
 function TGame.Start: Boolean;
+var
+  I: Integer;
+  J: Integer;
 begin
   FScore := 0;
   FRolls := 0;
+  FFrame := 1;
+
+  // ALE 20190801 consider moving Frame clearer to its own function or class
+  for I := 1 to FRAMES_TOTAL do
+  begin
+    FFrames[I].number := I;
+    FFrames[I].rolls := 0;
+    for J := 1 to ROLLS_PER_FRAME_MAX do
+    begin
+      FFrames[I].roll[J] := -1;
+    end;
+    FFrames[I].score := -1;
+  end;
+
   result := True;
 end;
 
